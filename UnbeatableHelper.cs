b3a93d67e4f51871b78e74cfe176e9e291b22736
Adris Jautakas <adrisj7@gmail.com>
2021-08-24 22:37:15 -0400

    In progress Edit UI + minor refactor

--- UnbeatableHelper.cs
+++ UnbeatableHelper.cs
@@ -5,9 +5,17 @@ namespace CustomBeatmaps
 {
     public static class UnbeatableHelper
     {
-        public static void PlayBeatmap(CustomBeatmapInfo beatmap, bool editMode = false)
+        public static void PlayBeatmap(CustomBeatmapInfo beatmap)
         {
             BeatmapParserLoadOverridePatch.SetOverrideBeatmap(beatmap);
+            OsuEditorPatch.SetEditMode(false);
+            LevelManager.LoadLevel("TEST_RHYTHM");
+        }
+
+        public static void PlayBeatmapEdit(CustomBeatmapInfo beatmap, string path)
+        {
+            BeatmapParserLoadOverridePatch.SetOverrideBeatmap(beatmap);
+            OsuEditorPatch.SetEditMode(true, path);
             LevelManager.LoadLevel("TEST_RHYTHM");
         }
     }
